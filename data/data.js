5ea5fc0667802eb3b0d968f887ac7b6fcce23ca1
Sreenidhi Bendre <67953001+sreenidhi3@users.noreply.github.com>
2021-11-27 18:35:18 +0530

    create: project set up

---
+++ data/data.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        id: 101,
+        productName: 'Women\'s Brown Winter Pack',
+        category: ['winter', 'women'],
+        description: 'A pack of of Sweater, TShirt, Boots and Pant. \nFabric – Cotton and Wool',
+        price: 2500,
+        url: 'https://images.unsplash.com/photo-1614676471928-2ed0ad1061a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2ludGVyJTIwY2xvdGhpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+
+    }, 
+    {
+        id: 102,
+        productName: 'Women\'s Woolen Knit Cap',
+        category: ['winter', 'women'],
+        description: 'A warm great quality knit cap in blue, red and purple colour threads.',
+        price: 500,
+        url: 'https://images.unsplash.com/photo-1607352208720-3f8883a35f9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHdvb2wlMjBjYXBzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    }, 
+    {
+        id: 103,
+        productName: 'Cute Bunny Women\'s Woolen Knit Socks',
+        category: ['winter', 'women'],
+        description: 'Warm cute looking bunny socks made of good quality wool.',
+        price: 300,
+        url: 'https://images.unsplash.com/photo-1514606991149-0c4c31d4e521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2ludGVyJTIwc29ja3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+
+    }, 
+    {
+        id: 104,
+        productName: 'Men\'s Woolen Knit Yellow Cap',
+        category: ['winter', 'men'],
+        description: 'Warm yellow cap made of good quality wool.',
+        price: 500,
+        url: 'https://images.unsplash.com/photo-1610935591850-9a3bf14810c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHdvb2wlMjBjYXBzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    }, 
+    {
+        id: 105,
+        productName: 'Women\'s Woolen Knit Red Sweater',
+        category: ['winter', 'women'],
+        description: 'Warm loose fit red sweater made of good quality wool.',
+        price: 700,
+        url: 'https://images.unsplash.com/photo-1608984361471-ff566593088f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3dlYXRlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+
+    },
+    {
+        id: 106,
+        productName: 'White Men\'s Hoodie',
+        category: ['winter', 'men'],
+        description: 'White casual loose fit drawstring pull over hoodie made of cotton mix.',
+        price: 500,
+        url: 'https://images.unsplash.com/photo-1598807381351-c2608db1c829?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdoaXRlJTIwaG9vZGllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    }, 
+    {
+        id: 107,
+        productName: 'Women\'s Brown Fur Coat',
+        category: ['winter', 'women'],
+        description: 'Warm fur over coat, loose fit brown colour made of good quality fur.',
+        price: 1500,
+        url: 'https://images.unsplash.com/photo-1580314030455-1d182ab1f4f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHN3ZWF0ZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    },
+    {
+        id: 108,
+        productName: 'Red and Brown Neck Scarf',
+        category: ['winter', 'women', 'men'],
+        description: 'Red and brown colour warm woolen neck scarf ideal for both men and women.',
+        price: 500,
+        url: 'https://images.unsplash.com/photo-1609803384069-19f3e5a70e75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2NhcmZzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    },
+    {
+        id: 109,
+        productName: 'Light Brown Jacket ',
+        category: ['winter', 'men'],
+        description: 'Casual light brown colour jacket with 5 pockets and a pull over attached cap.',
+        price: 900,
+        url: 'https://images.unsplash.com/photo-1536148020659-4dde4128f8ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG1lbidzJTIwd2ludGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+
+    },
+    {
+        id: 110 ,
+        productName: 'Men\'s Leather Coat ',
+        category: ['winter', 'men'],
+        description: 'Casual brown colour regular fit leather jacket.',
+        price: 1200,
+        url: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVuJ3MlMjB3aW50ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+
+    }
+]
